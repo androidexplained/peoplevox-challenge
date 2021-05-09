ec0fa48306e790de6a5f8f27ceb9331992e89712
dassem.ultor.studio <dassem.ultor.studio@gmail.com>
2021-05-09 12:20:49 +0200

    Create abstraction over fixedRateTimer

---
+++ app/src/main/java/com/sample/randomscanner/domain/Timer.kt
@@ -0,0 +1,24 @@
+package com.sample.randomscanner.domain
+
+import javax.inject.Inject
+import kotlin.concurrent.fixedRateTimer
+
+class FixedRateTimer @Inject constructor() : Timer {
+
+    private lateinit var timer: java.util.Timer
+
+    override fun start(interval: Long, action: () -> Unit) {
+        timer = fixedRateTimer("timer", false, 0, interval) { action() }
+    }
+
+    override fun cancel() {
+        timer.cancel()
+    }
+
+}
+
+interface Timer {
+    fun start(interval: Long, action: () -> Unit)
+    fun cancel()
+}
+
