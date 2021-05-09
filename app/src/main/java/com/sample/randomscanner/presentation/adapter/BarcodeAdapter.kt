61d6bcc74a8fd9e3be52ffdcd47bcae3875dd670
dassem.ultor.studio <dassem.ultor.studio@gmail.com>
2021-05-09 12:22:27 +0200

    Create BarcodeAdapter

---
+++ app/src/main/java/com/sample/randomscanner/presentation/adapter/BarcodeAdapter.kt
@@ -0,0 +1,49 @@
+package com.sample.randomscanner.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sample.randomscanner.databinding.BarcodeItemBinding
+import com.sample.randomscanner.domain.model.Barcode
+
+class BarcodeAdapter(private val onItemClick: (Barcode) -> Unit) :
+    RecyclerView.Adapter<BarcodeAdapter.ViewHolder>() {
+
+    private var barcodes = mutableListOf<Barcode>()
+
+    fun addBarcode(barcode: Barcode) {
+        barcodes.add(barcode)
+        barcodes = barcodes.sortedBy { it.name }.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            BarcodeItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding) { onItemClick(barcodes[it]) }
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.onBind(barcodes[position])
+    }
+
+    override fun getItemCount() = barcodes.size
+
+    class ViewHolder(private val binding: BarcodeItemBinding, onItemClick: (Int) -> Unit) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener { onItemClick(adapterPosition) }
+        }
+
+        fun onBind(barcode: Barcode) {
+            binding.run {
+                name.text = barcode.name
+                name.setTextColor(barcode.color.resource)
+                type.text = barcode.type
+                date.text = barcode.date
+            }
+        }
+    }
+}
